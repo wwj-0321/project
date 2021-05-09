fe04734586b604528b35f091573db7c517cbdebe
wwj-0321 <2844974769@qq.com>
2021-05-09 23:57:11 +0800

    123

---
+++ src/components/power/Rloes.vue
@@ -0,0 +1,236 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <!--添加角色按钮区域  -->
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+      <!-- 角色列表区域 -->
+      <el-table :data="rolelist" border stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+            <el-row
+              :class="['bdbuttom', i1 === 0 ? 'bdtop' : '', 'vcenter']"
+              v-for="(item1, i1) in scope.row.children"
+              :key="item1.id"
+            >
+              <!-- 一级 -->
+              <el-col :span="5">
+                <el-tag
+                  closable
+                  @close="removeRightById(scope.row, item1.id)"
+                  >{{ item1.authName }}</el-tag
+                >
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 二级和三级 -->
+              <el-col :span="19">
+                <!-- <pre>{{scope.row}}</pre> -->
+                <el-row
+                  :class="[i2 === 0 ? '' : 'bdtop', 'vcenter']"
+                  v-for="(item2, i2) in item1.children"
+                  :key="item2.id"
+                >
+                  <el-col :span="6">
+                    <el-tag
+                      closable
+                      type="success"
+                      @close="removeRightById(scope.row, item2.id)"
+                      >{{ item2.authName }}</el-tag
+                    >
+                    <i class="el-icon-caret-right"></i
+                  ></el-col>
+                  <el-col :span="18">
+                    <el-tag
+                      closable
+                      type="warning"
+                      v-for="item3 in item2.children"
+                      :key="item3.id"
+                      @close="removeRightById(scope.row, item3.id)"
+                      >{{ item3.authName }}</el-tag
+                    >
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作" width="300px">
+          <template slot-scope="scope">
+            <!-- 修改按钮  -->
+            <el-button type="primary" icon="el-icon-edit" size="mini"
+              >编辑</el-button
+            >
+            <!-- 删除按钮 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini"
+              >删除</el-button
+            >
+            <!-- 分配角色按钮 -->
+            <el-button
+              type="warning"
+              icon="el-icon-setting"
+              size="mini"
+              @click="showSetRightDialog(scope.row)"
+              >分配权限</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+    <el-dialog
+      title="分配权限"
+      :visible.sync="setRightDialogVisible"
+      width="50%"
+      @close="setRightDialogClose"
+    >
+      <!-- 树形控件 -->
+      <el-tree
+        show-checkbox
+        default-expand-all
+        :default-checked-keys="defkeys"
+        ref='treeRef'
+        node-key="id"
+        :data="rightslist"
+        :props="treeProps"
+      ></el-tree>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRightDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="allotRights">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      // 所有角色列表数据
+      rolelist: [],
+      setRightDialogVisible: false,
+      // 所有的权限数据
+      rightslist: [],
+      // 树形控件属性绑定
+      treeProps: {
+        children: 'children',
+        label: 'authName',
+      },
+      defkeys: [],
+      roleId: [],
+    }
+  },
+  created() {
+    this.getRolelist()
+  },
+  methods: {
+    // 分配权限
+    async showSetRightDialog(role) {
+      this.roleId = role.id
+      const { data: res } = await this.$http.get('rights/tree')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取权限数据失败')
+      }
+      this.rightslist = res.data
+      //  console.log(this.rightslist)
+      this.getLeafKeys(role, this.defkeys)
+      this.setRightDialogVisible = true
+    },
+    getLeafKeys(node, arr) {
+      if (!node.children) {
+        return arr.push(node.id)
+      }
+      node.children.forEach(
+        (item) => this.getLeafKeys(item, arr)
+        // forEach 方法遍历数组  三个参数 数组元素 item
+        // 数组索引index 数组本身arr
+      )
+    },
+    // 确定时获取keys(半勾选和全部勾选的id)以及发送请求
+ async   allotRights() {
+      const keys=[
+        ...this.$refs.treeRef.getCheckedKeys(),
+        ...this.$refs.treeRef.getHalfCheckedKeys()
+]
+// console.log(keys)
+        const idStr =keys.join(',')
+const {data:res}  =await   this.$http.post(`roles/${this.roleId}/rights`,
+        {rids:idStr})
+        if(res.meta.status!==200){return this.$message.error('添加权限失败')}
+        this.getRolelist()
+        this.setRightDialogVisible=false
+        this.$message.success('添加权限成功')
+    },
+    // 关闭时重置数组 防止数组获取叠加
+    setRightDialogClose() {
+      this.defkeys = []
+    },
+    async removeRightById(role, rightId) {
+      // 弹框提示是否删除
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该文件, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning',
+        }
+      ).catch((err) => err)
+      // 如果用户确认删除，则返回值为字符串 confirm
+      // 如果用户取消了删除，则返回值为字符串 cancel
+      // console.log(confirmResult)
+      if (confirmResult !== 'confirm') {
+        return this.$message.error('取消了删除')
+      }
+      // console.log('确定删除')
+      const { data: res } = await this.$http.delete(
+        `roles/${role.id}/rights/${rightId}`
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除失败')
+      }
+      console.log(res)
+      console.log(role)
+      // this.getRolelist()这个会让其重新渲染 这时候只是改变了data的数据 可以下列操作
+      role.children = res.data
+      //  this.$message.success('删除成功')
+    },
+
+    async getRolelist() {
+      const { data: res } = await this.$http.get('roles')
+      //   console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.rolelist = res.data
+      this.$message.success('获取角色列表成功')
+    },
+  },
+}
+</script>
+<style land="less" scoped>
+.el-tag {
+  margin: 7px;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.bdbuttom {
+  border-bottom: 1px solid #eee;
+}
+.vcenter {
+  display: flex;
+  align-items: center;
+}
+</style>
