ad22304fcf281d36ead091910aeb4bb8310eb1ad
wwj-0321 <2844974769@qq.com>
2022-05-23 01:29:07 +0800

    tijiao

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>分类参数</div>
+    <div>
+         <!-- 面包屑  -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    </div>
 </template> 
 <script>
 export default {
